468280a32347e95873d73c0a3b524b774925119a
Imperadorvader97 <65308207+Imperadorvader97@users.noreply.github.com>
2021-07-28 16:12:25 -0300

    Update Joguinho.py

--- Joguinho.py
+++ Joguinho.py
@@ -1,29 +1,39 @@
 print("Bem-vindo ao jogo da adivinhação!")
-numero_secreto = 5
-tentativas = 3
+import random
 
-while(tentativas > 0):
-    chute_str = input("Digite o seu número: ")
-    print("você digitou " + chute_str)
-    chute = int(chute_str)
+escolha_replay = 1
+while(escolha_replay == 1):
+        print("Adivinhe o número secreto de 1 até 10!")
+        numero_secreto = random.randint(1, 10)
+        tentativas = 3
+        while(tentativas > 0):
+            chute_str = input("Digite o seu número: ")
+            print("Você digitou " + chute_str + "!")
+            chute = int(chute_str)
 
-    acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+            acertou = chute == numero_secreto
+            maior = chute > numero_secreto
+            menor = chute < numero_secreto
 
-    if (acertou):
-        print("Acertou!!!")
-        break
-    else:
-        if (maior):
-            print("Seu chute foi muito alto!")
-        elif (menor):
-            print("Seu chute foi muito baixo!")
+            if (acertou):
+                print("Acertou!!!")
+                break
 
-    tentativas = tentativas - 1
-    tentativas_str = str(tentativas)
-    print("Tentativas restantes:" + tentativas_str)
+            else:
+                if (maior):
+                    print("Seu chute foi muito alto!")
+                elif (menor):
+                    print("Seu chute foi muito baixo!")
 
-escolha_replay = input("Quer jogar de novo? Se sim, digite 1. Se não, digite 0.")
+                tentativas = tentativas - 1
+                tentativas_str = str(tentativas)
+                print("Tentativas restantes: " + tentativas_str)
 
-print("Fim do jogo :)")
+        print("O número correto era o: " + str(numero_secreto))
+        escolha_replay = int(input("Deseja jogar de novo? Digite 1 se sim. Se não, digite 0."))
+        if(escolha_replay == 1):
+            tentativas = 3
+            print("\n" * 2)
+
+
+print("Fim de Jogo! :)")
